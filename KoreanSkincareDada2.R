be6586be65422bd5adcacab9d251c5ecf85a23dd
madelinehakala <117949834+madelinehakala@users.noreply.github.com>
2022-12-08 11:48:25 -0600

    Update KoreanSkincareDada2.R

--- KoreanSkincareDada2.R
+++ KoreanSkincareDada2.R
@@ -7,26 +7,18 @@
 #BiocManager::install("dada2", version = "3.15")
 
 
-#troubleshoot phyloseq object + visualizations (alpha diversity looks funky)
-
-save.image(file = "120622.Rdata")
-#load("120622.Rdata")
-
-
 # Load dada2 and check the version
 library("dada2"); packageVersion("dada2")
 
-# UPDATED FROM TUTORIAL
 # Make a variable that lists all the files in the KoreanSkincareTrimmedReads_g folder and then list them.
 path <- "/cloud/project/KoreanSkincareTrimmedReads_g" # using option g right now
 list.files(path)
 
-# UPDATED FROM TUTORIAL
+
 # Forward and reverse fastq filenames end in _1.fastq.gz and _2.fastq.gz
 fnFs <- sort(list.files(path, pattern="_1.fastq.gz", full.names = TRUE))
 fnRs <- sort(list.files(path, pattern="_2.fastq.gz", full.names = TRUE))
-# UPDATED FROM TUTORIAL
-# Just changed 1 to 2 to get the info after the first underscore
+# Changed 1 (tutorial value) to 2 to extract the info after the first underscore
 # Extract sample names, assuming filenames start with Trimmed_SAMPLENAME
 sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`, 2)
 
@@ -74,7 +66,7 @@ dim(seqtab)
 # Inspect distribution of sequence lengths
 table(nchar(getSequences(seqtab)))
 
-# Remove bimers and print dimensions
+# Remove bimera and print dimensions
 seqtab.nochim <- removeBimeraDenovo(seqtab, method="consensus", multithread=FALSE, verbose=TRUE)
 dim(seqtab.nochim)
 
@@ -94,7 +86,6 @@ head(track)
 
 # Now begin Part 2 (Assigning taxonomy)
 
-# READS MIGHT NOT BE APPROPRIATELY ASSIGNED: WILL DISCUSS DURING MEETING
 # Need to upload the silva_nr_v132_train_set.fa.gz file into the KoreanSkincareTrimmedReads_g folder
 # Assign taxonomy using SILVA database
 taxa <- assignTaxonomy(seqtab.nochim, "/cloud/project/KoreanSkincareTrimmedReads_g/silva_nr_v132_train_set.fa.gz", multithread=FALSE)
@@ -113,22 +104,6 @@ head(taxa.print)
 # Just to see what we're working with
 rownames(seqtab.nochim)
 
-# I BELIEVE THIS CODE IS ONLY FOR THE TUTORIAL
-# WE DO NOT HAVE A MOCK COMMUNITY TO EVALUATE DADA2'S ACCURACY AGAINST
-# Subset to mock samples
-# unqs.mock <- seqtab.nochim["Mock",]
-# unqs.mock <- sort(unqs.mock[unqs.mock>0], decreasing=TRUE) # Drop ASVs absent in the Mock, sort decreasing
-# Print number of exact matches
-# cat("DADA2 inferred", length(unqs.mock), "sample sequences present in the Mock community.\n")
-# Load mock reference
-# mock.ref <- getSequences(file.path(path, "HMP_MOCK.v35.fasta"))
-# Count number of unique mock species in ref
-# match.ref <- sum(sapply(names(unqs.mock), function(x) any(grepl(x, mock.ref))))
-# Print number of exact matches
-# cat("Of those,", sum(match.ref), "were exact matches to the expected reference sequences.\n")
-
-# THE CODE BELOW THIS IS PRETTY SPECIFIC TO THE TUTORIAL
-# OUR FILES ARE NAMED VERY DIFFERENTLY, SO WE'D NEED TO GENERATE PLOTS IN A DIFFERENT WAY
 # Load packages
 library(phyloseq); packageVersion("phyloseq")
 library(Biostrings); packageVersion("Biostrings")
@@ -141,6 +116,8 @@ theme_set(theme_bw())
 metadata <- read.table(file = 'Metadata.txt', sep = ',', header = TRUE)
 View(metadata)
 
+# This makes a new metadata file that contains week as a column name since our metadata file doesn't explicitly list week
+# This step is not required for most metadata files
 samples.out <- rownames(seqtab.nochim)
 subject <- substr(metadata$Submitter_Id, 6,8)
 week <- substr(metadata$Submitter_Id, 4,4)
@@ -149,18 +126,24 @@ rownames(samdf) <- samples.out
 
 
 # Setting phyloseq object
-ps_data <- phyloseq(otu_table(seqtab.nochim, taxa_are_rows=FALSE), sample_names(samdf), 
-                tax_table(taxa))
+# otu_table is seqtab.nochim
+# sample_data is metadata
+# tax_table is taxa
+ps_data <- phyloseq(otu_table(seqtab.nochim, taxa_are_rows=FALSE), sample_data(samdf), 
+                    tax_table(taxa))
 
-#alpha diversity
 
-plot_richness(ps_data)
+# Plotting alpha diversity
+# Use phyloseq object "ps_data"
+# Set x to the variable we are interested in (week)
+# Chose to only do Simpson and Shannon plots 
+plot_richness(ps_data, x="Week", color="Week", measures=c("Simpson", "Shannon")) +
+  guides(color = "none") +
+  theme(axis.text.x = element_text(angle = 0))
 
-#bar plot
-
+# Bar plot
+# x = Week
 top20 <- names(sort(taxa_sums(ps_data), decreasing=TRUE))[1:20]
 ps.top20 <- transform_sample_counts(ps_data, function(OTU) OTU/sum(OTU))
 ps.top20 <- prune_taxa(top20, ps.top20)
-plot_bar(ps.top20, fill="Family") #+ facet_wrap(~When, scales="free_x")
-
-
+plot_bar(ps.top20, x = "Week", fill="Family")
